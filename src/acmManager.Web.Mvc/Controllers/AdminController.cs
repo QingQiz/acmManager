3aa16b460630dfb0dee605cc4caf0038861732af
QingQiz <sofeeys@outlook.com>
2020-09-27 23:12:51 +0800

    refactor: index for Admin

---
+++ src/acmManager.Web.Mvc/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
+using acmManager.Authorization;
+using acmManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acmManager.Web.Controllers
+{
+    [AbpMvcAuthorize(PermissionNames.PagesUsers_Admin)]
+    public class AdminController : acmManagerControllerBase
+    {
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
