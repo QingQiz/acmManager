cd9a06f59666e336e9824aea7d2e0e4d68145007
QingQiz <sofeeys@outlook.com>
2020-09-27 17:54:21 +0800

    refactor: 重构了cshtml的结构，使用 Html.Partial 简化 Html

--- src/acmManager.Web.Mvc/Models/Users/UpdateFromAoxiangViewModel.cs
+++ src/acmManager.Web.Mvc/Models/Users/UpdateFromAoxiangViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace acmManager.Web.Models.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace acmManager.Web.Models.Users
 {
     public class UpdateFromAoxiangViewModel
     {
+        [Required(AllowEmptyStrings = false)]
         public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string PasswordAgain { get; set; }
     }
 }
