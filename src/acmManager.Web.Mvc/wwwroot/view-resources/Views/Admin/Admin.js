9e0b00d08066d1f02b9df02eb540a0bc2ef16908
QingQiz <sofeeys@outlook.com>
2020-10-01 01:04:26 +0800

    refactor: 使用 class 作为 pagination 的选择器（而不是 id）

--- src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Admin/Admin.js
+++ src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Admin/Admin.js
@@ -1,33 +1,33 @@
 ﻿// Pagination
 let getAllUserPaginationEvent = function () {
-    let pageSize = parseInt($('#get-all-user-filter-MaxResultCount').val());
-    let skipCount = parseInt($('#get-all-user-filter-skip-count').val());
+    let pageSize = parseInt($('#user-management .get-all-user-filter-MaxResultCount').val());
+    let skipCount = parseInt($('#user-management .get-all-user-filter-skip-count').val());
     let pageIndex = Math.floor(skipCount / pageSize) + 1;
 
     let gotoPage = function (n) {
-        $('#get-all-user-filter-skip-count').val((n - 1) * pageSize);
-        $('#get-all-user-filter-submit-btn').click();
+        $('#user-management .get-all-user-filter-skip-count').val((n - 1) * pageSize);
+        $('#user-management .get-all-user-filter-submit-btn').click();
     }
 
-    $('.get-all-user-page-link').click(function () {
+    $('#get-all-user-table .page-index').click(function () {
         let page = parseInt($(this).text());
         gotoPage(page);
     });
     
-    $('#get-all-user-page-next').click(function () {
+    $('#get-all-user-table .page-next').click(function () {
         gotoPage(pageIndex + 1);
     });
 
-    $('#get-all-user-page-previous').click(function () {
+    $('#get-all-user-table .page-previous').click(function () {
         gotoPage(pageIndex - 1);
     });
 
-    $('#get-all-user-page-first').click(function () {
+    $('#get-all-user-table .page-first').click(function () {
         gotoPage(1);
     });
 }
 
-$('#get-all-user-filter-submit-btn').click(function () {
+$('#user-management .get-all-user-filter-submit-btn').click(function () {
     let form = $('#get-all-user-filter-form');
     
     $.ajax({
@@ -43,5 +43,5 @@ $('#get-all-user-filter-submit-btn').click(function () {
 });
 
 $(function () {
-    $('#get-all-user-filter-submit-btn').click();
+    $('#user-management .get-all-user-filter-submit-btn').click();
 });
