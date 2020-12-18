43d8b73900a6fe583e7e8580af6035eb202a4f95
QingQiz <sofeeys@outlook.com>
2020-12-18 18:26:09 +0800

    feat: frontend: ContestController: index

---
+++ src/acmManager.Web.Mvc/Controllers/ContestController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using acmManager.Contest;
+using acmManager.Controllers;
+using acmManager.Web.Models.Contest;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acmManager.Web.Controllers
+{
+    public class ContestController : acmManagerControllerBase
+    {
+        private readonly ContestAppService _contestAppService;
+
+        public ContestController(ContestAppService contestAppService)
+        {
+            _contestAppService = contestAppService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var contestList = await _contestAppService.GetContestListAsync();
+            return View("Index", new ContestListViewModel(contestList));
+        }
+
+        [HttpGet, Route("/Contest/{contestId}")]
+        public async Task<ActionResult> GetContest(long contestId)
+        {
+            var contest = await _contestAppService.GetContestAsync(contestId);
+            return View("Contest", contest);
+        }
+
+        [HttpGet, Route("/Contest/Create")]
+        public ActionResult CreateContestView()
+        {
+            return View("CreateContest");
+        }
+        
+    }
+}
---
+++ src/acmManager.Web.Mvc/Models/Contest/ContestListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using acmManager.Contest.Dto;
+
+namespace acmManager.Web.Models.Contest
+{
+    public class ContestListViewModel
+    {
+        public List<GetContestListOutput> Contests { get; set; }
+
+        public ContestListViewModel(List<GetContestListOutput> input)
+        {
+            Contests = input;
+        }
+    }
+}
