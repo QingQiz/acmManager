a1cb0c85ed58d6cf7ab0b5697689958a8a4feb3e
QingQiz <sofeeys@outlook.com>
2021-02-11 01:40:59 +0800

    feat: frontend: get solution

---
+++ src/acmManager.Web.Mvc/Models/Shared/CreatorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace acmManager.Web.Models.Shared
+{
+    public class CreatorViewModel
+    {
+        public long CreatorId { get; set; }
+    }
+}
---
+++ src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Problem/Solution.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    previewMd("problem-description", $('#problem-description > textarea').text());
+    previewMd("solution-content", $('#solution-content > textarea').text());
+})
