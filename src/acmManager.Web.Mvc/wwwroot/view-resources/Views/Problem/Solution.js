06fb265a2d9fcc9e4629fdf198772b72ae91b936
QingQiz <sofeeys@outlook.com>
2021-02-13 01:09:26 +0800

    feat: frontend: list root comment

--- src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Problem/Solution.js
+++ src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Problem/Solution.js
@@ -3,4 +3,9 @@
     
     previewMd("problem-description", $('#problem-description > textarea').text());
     previewMd("solution-content", $('#solution-content > textarea').text());
+    
+    $('.comments').each(function () {
+        let id = $(this).attr('id');
+        previewMd(id, $(this).find('textarea').text());
+    })
 })
