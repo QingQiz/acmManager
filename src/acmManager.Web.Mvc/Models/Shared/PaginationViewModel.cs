7a96fa3cbe20e0fd42ca9515cee67c5cc0b1a223
QingQiz <sofeeys@outlook.com>
2021-02-12 22:09:22 +0800

    refactor: frontend: refactor pagination

---
+++ src/acmManager.Web.Mvc/Models/Shared/PaginationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace acmManager.Web.Models.Shared
+{
+    public class PaginationViewModel
+    {
+        public int MaxResultCount { get; set; }
+        public int SkipCount { get; set; }
+        
+        public long AllResultCount { get; set; }
+    }
+}
