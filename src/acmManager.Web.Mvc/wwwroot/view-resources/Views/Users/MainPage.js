9b276285fc9e1298e7ac5675440e4f8fdd117d80
QingQiz <sofeeys@outlook.com>
2021-02-14 18:07:38 +0800

    feat: solution count chart

--- src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Users/MainPage.js
+++ src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Users/MainPage.js
@@ -8,28 +8,29 @@
     yellow: "rgb(255, 205, 86)",
 };
 
-$(function () {
-    let labels = $('input[name="problem-types"]').val().split(',').map(s => s.split(':'));
-    
-    let color = Chart.helpers.color;
-    
-    let radarChart = new Chart($('#radar-chart')[0], {
+let color = Chart.helpers.color;
+
+let radarChart = function (ctx, title, labelTitle, labelToData) {
+    return new Chart(ctx, {
         type: 'radar',
         data: {
-            labels: labels.map(l => l[0]),
+            labels: labelToData.map(l => l[0]),
             datasets: [{
-                label: "题目数量",
-                data: labels.map(l => parseInt(l[1])),
+                label: labelTitle,
+                data: labelToData.map(l => parseInt(l[1])),
                 backgroundColor: color(chartColors.red).alpha(0.2).rgbString(),
                 borderColor: color(chartColors.red).alpha(0.7).rgbString()
             }]
         },
         options: {
-            responsive: false,
+            responsive: true,
             aspectRatio: 1,
+            legend: {
+                display: false
+            },
             title: {
                 display: true,
-                text: '题目类型统计',
+                text: title,
                 fontSize: 15
             },
             scale: {
@@ -39,9 +40,67 @@ $(function () {
             },
             tooltips: {
                 callbacks: {
-                    title: (item, data) => data.labels[item[0].index]
+                    title: (item, data) => item.map(i => data.labels[i.index]).join(',')
                 }
             }
         }
     });
+};
+
+let lineChart = function (ctx, title, labelTitle, labelToData) {
+    return new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: labelTitle,
+                data: labelToData.map(l => {
+                    return {
+                        t: new Date(l[0]), y: parseInt(l[1])
+                    };
+                })
+                // backgroundColor: color(chartColors.red).alpha(0.2).rgbString(),
+                // borderColor: color(chartColors.red).alpha(0.7).rgbString()
+            }]
+        },
+        options: {
+            responsive: true,
+            aspectRatio: 2,
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: title,
+                fontSize: 15
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        displayFormats: {
+                            'day': 'YYYY-MM-DD'
+                        }
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        stepSize: 1
+                    }
+                }]
+            }
+        }
+    });
+};
+
+
+$(function () {
+    let labelsProblemTypes = $('input[name="problem-types"]').val().split(',').map(s => s.split(':'));
+    
+    radarChart($('#radar-chart')[0], '题解——题目类型统计', "题目数量", labelsProblemTypes);
+    
+    let labelSolutionCount = $('input[name="problem-count"]').val().split(',').map(s => s.split('~'));
+    
+    lineChart($('#solution-count-chart')[0], '题解——数量统计', "题解总数", labelSolutionCount);
 });
