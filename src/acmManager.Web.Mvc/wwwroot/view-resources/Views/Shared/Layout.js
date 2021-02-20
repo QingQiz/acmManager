27c0e20b25a84e20d53158595f6b1813fe367f90
QingQiz <sofeeys@outlook.com>
2021-02-20 23:02:16 +0800

    feat: add baidu data analysis

--- src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Shared/Layout.js
+++ src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Shared/Layout.js
@@ -3,3 +3,11 @@ $('.card-header').click(function (event) {
     $(target).collapse('toggle');
 });
 
+// 百度访问统计
+let _hmt = _hmt || [];
+(function() {
+    let hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?686023326bba34e1a6c8c701758124b6";
+    let s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
