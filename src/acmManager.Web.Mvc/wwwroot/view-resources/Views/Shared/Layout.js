1d4a4df887b0de3c40b0d2f4f76dc406ac61dc66
QingQiz <sofeeys@outlook.com>
2021-02-28 02:14:51 +0800

    feat: frontend: scroll to top

--- src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Shared/Layout.js
+++ src/acmManager.Web.Mvc/wwwroot/view-resources/Views/Shared/Layout.js
@@ -4,10 +4,32 @@ $('.card-header').click(function (event) {
 });
 
 // 百度访问统计
-let _hmt = _hmt || [];
+var _hmt = _hmt || [];
 (function() {
     let hm = document.createElement("script");
     hm.src = "https://hm.baidu.com/hm.js?686023326bba34e1a6c8c701758124b6";
     let s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
+
+// scroll to top button
+$(function () {
+    $(window).scroll(function () {
+        let w = $(this);
+        
+        if (w.width() < 800) return;
+        
+        if (w.scrollTop() > 50) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });
+    
+    $('#scroll-to-top').click(function () {
+        $('html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
